949e67c504b16fce8f808d503426ee92db7de4a7
Juan Gonzalez <juan@sancrisoft.com>
2018-10-26 17:51:08 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+    }),
+  ],
+}
