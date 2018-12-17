9d5ccc055c8793134e6a9421a5807312eb958ff6
Johan Moncada <johan@sancrisoft.com>
2018-12-17 10:40:26 -0500

    Header review

--- app/components/Header/styledComponents.js
+++ app/components/Header/styledComponents.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 import breakpoint from 'styled-components-breakpoint'
 
+// En el caso de a y h1 se recomienda crear un nuevo styledComponent,
+// Utilizar ese tipo de maquetación cuando son librerias y no podamos modificar la estructura
+// Aqui quedarian entonces HeaderWrapper, Link, Title.
 const HeaderWrapper = styled.header`
   background: #ff6335;
   height: 10vh;
@@ -12,7 +15,7 @@ const HeaderWrapper = styled.header`
   a {
     text-decoration: none;
     color: #fff;
-    
+
     h1 {
       font-size: 30px;
       font-weight: bold;
